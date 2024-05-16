1ebe4a01e4a4ec7f0c34ddc601dc8aa7ff2aec46
WenJenChun <daydreamsss@hotmail.com>
2024-05-16 13:56:32 +0800

    add thing and travelmate section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import Nav from "./Nav";
 import Banner from "./Banner";
 import Search from "./Search";
 import Room from "./Room"
-import Counter from "./Counter";
+import Thing from "./Thing"
+import TravelMate from "./TravelMate"
+// import Counter from "./Counter";
 
 function App() {
   return (
@@ -13,6 +15,14 @@ function App() {
      <div className="px-20 pt-8"><h2 className="text-2xl px-3">Room Share</h2></div>
      <div className="px-20">     
         <Room imgSrc="https://images.unsplash.com/photo-1554995207-c18c203602cb?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+     </div>
+     <div className="px-20 pt-8"><h2 className="text-2xl px-3">愛物惜物 二手物找新家</h2></div>
+     <div className="px-20">     
+        <Thing imgSrc="https://images.unsplash.com/photo-1503926359680-9ddd5b2bcbdc?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+     </div>
+     <div className="px-20 pt-8"><h2 className="text-2xl px-3">一起出去玩</h2></div>
+     <div className="px-20">     
+        <Thing imgSrc="https://images.unsplash.com/photo-1527002836355-e11c5be50cf8?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
      </div>
 
      
--- src/Thing.js
+++ src/Thing.js
@@ -3,15 +3,15 @@ import { LuBedDouble } from "react-icons/lu";
 import { LuPoundSterling } from "react-icons/lu";
 
 
-const Room = ({ imgSrc }) => {
+const Thing = ({ imgSrc }) => {
   return (
     <div className='w-56 m-3'>
       <img className="rounded-xl h-56 object-cover" src={imgSrc} alt="Modern building architecture" />
-      <p className='font-bold py-1'>市中心乾淨明亮通風雙人房，...</p>
+      <p className='font-bold py-1'>全新大同電鍋，可煮十人份的..</p>
       <div className="flex justify-between text-sm">
-        <p>長租</p>
-        <div ><LuBedDouble className=' inline' size={20}/><span>Single</span></div>
-        <div><LuPoundSterling className=' inline' size={18}/><p className='pt-1 inline'>675 pcm</p></div>
+        <p>全新</p>
+        {/* <div ><LuBedDouble className=' inline' size={20}/><span>Single</span></div> */}
+        <div><LuPoundSterling className=' inline' size={18}/><p className='pt-1 inline'>105</p></div>
       </div>
     </div>
       
@@ -19,4 +19,4 @@ const Room = ({ imgSrc }) => {
   );
 };
 
-export default Room;
+export default Thing;
