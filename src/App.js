01ab943bd70752019b9d6e1bb9eebbc1adb44fc1
WenJenChun <daydreamsss@hotmail.com>
2024-05-30 01:29:05 +0800

    modify index layout and add footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TravelMate from "./TravelMate"
 import SeeMore from "./SeeMore";
 import TextImageBox from "./TextImageBox"
 import TextBox from "./TextBox"
+import Footer from "./Footer"
 // import Counter from "./Counter";
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
      <Nav />
      <Banner src="images/banner.jpg" />
      <Search />
-     <div className="container mx-auto mt-5 max-w-screen-xl">
+     <div className="container mx-auto mt-9 max-w-screen-xl  px-4 sm:px-6 lg:px-8">
          <h2 className="text-2xl text-center sm:text-left ">Room Share</h2>
          <div className="flex flex-wrap justify-center md:justify-start mt-5">
             <Room imgSrc="https://images.unsplash.com/photo-1554995207-c18c203602cb?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
@@ -29,7 +30,7 @@ function App() {
          <SeeMore className="" pageTitle="房源"/>
       </div>
      </div>
-     <div className="container mx-auto mt-5 max-w-screen-xl">
+     <div className="container mx-auto mt-9 max-w-screen-xl  px-4 sm:px-6 lg:px-8">
          <h2 className="text-2xl text-center sm:text-left ">愛物惜物 二手物找新家</h2>
          <div className="flex flex-wrap justify-center md:justify-start mt-5">
             <Thing imgSrc="https://images.unsplash.com/photo-1503926359680-9ddd5b2bcbdc?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
@@ -43,7 +44,7 @@ function App() {
          <SeeMore className="" pageTitle="二手物"/>
       </div>
      </div>
-     <div className="container mx-auto mt-5 max-w-screen-xl	">
+     <div className="container mx-auto mt-9 max-w-screen-xl	 px-4 sm:px-6 lg:px-8">
          <h2 className="text-2xl text-center sm:text-left ">一起出去玩</h2>
          <div className="flex flex-wrap justify-center md:justify-start mt-5">
             <TravelMate imgSrc="https://images.unsplash.com/photo-1527002836355-e11c5be50cf8?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
@@ -57,7 +58,7 @@ function App() {
             <SeeMore className="" pageTitle="出遊提案"/>
          </div>
      </div>
-     <div className="container mx-auto mt-5 max-w-screen-xl">
+     <div className="container mx-auto mt-9 max-w-screen-xl px-4 sm:px-6 lg:px-8">
       <h2 className="text-2xl text-center sm:text-left mb-3">大家都在看</h2>
       <div className="flex flex-wrap md:flex-nowrap justify-center">
          <TextImageBox text="代購" textUrl="https://translate.google.com/"/>
@@ -68,18 +69,18 @@ function App() {
          <TextImageBox text="行李空間"/>
       </div>
      </div>
-     <div className="container mx-auto mt-5 max-w-screen-xl">
+     <div className="container mx-auto mt-5 max-w-screen-xl px-4 sm:px-6 lg:px-8">
          <h2 className="text-2xl text-center sm:text-left mb-3">我也想知道</h2>
-         <div className="flex flex-wrap justify-start">
+         <div className="flex flex-wrap  md:flex-nowrap justify-center">
             <TextBox text="Boots 打折時段?" textUrl="https://translate.google.com/"/>
             <TextBox text="可以 walk in 的 GP ?"/>
             <TextBox text="東北大米好吃嗎?"/>
             <TextBox text="倫敦治安好嗎？"/>
             <TextBox text="歐洲之星搭到法國的問題"/>
             <TextBox text="在英國當按摩師執業需要證照？"/>
-            <TextBox text="推薦哪家的登山鞋？"/>
          </div>
       </div>
+      <Footer />
 
      
     
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-whiteOrange py-4 text-center mt-12 text-white'>© 2023 JUNEW. All rights reserved.</div>
+  )
+}
+
+export default Footer
