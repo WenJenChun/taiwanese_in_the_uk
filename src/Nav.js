f49a1166da7c6079cb7c38daab2fbdcc698ea933
WenJenChun <daydreamsss@hotmail.com>
2024-06-02 00:39:19 +0800

    add navbar toggle for mobile and add gsap

--- src/Nav.js
+++ src/Nav.js
@@ -1,85 +1,86 @@
-import React, {useEffect} from 'react'
-import "./styles/style.css"
+import React, { useState } from 'react';
+import "./styles/style.css";
 import { BsCupHot } from 'react-icons/bs';
 import { PiHamburgerThin } from "react-icons/pi";
-// import { query } from 'express';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 
 
 const Nav = () => {
-  useEffect(() => {
-    const hamburgerIcon = document.querySelector("#hamburger");
-    const menuChoice = document.querySelector("#menuChoice");
+  const [isPhoneMenuVisible, setPhoneMenuVisible] = useState(false);
+  const togglePhoneMenu = () => {
+    setPhoneMenuVisible(!isPhoneMenuVisible);
+  };
 
-    if (hamburgerIcon) {
-      hamburgerIcon.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log("click");
-        menuChoice.classList.add("text-green");
+//   gsap.registerPlugin(useGSAP);
 
-        // 這裡可以添加更多處理邏輯
-      });
-    }
-    // 清除事件監聽器
-    return () => {
-      if (hamburgerIcon) {
-        hamburgerIcon.removeEventListener("click", () => {
-          console.log("click");
-        });
-      }
-    };
-  }, []);
+// const container = useRef();
+
+// useGSAP(
+//     () => {
+//         // gsap code here...
+//         gsap.to('.box', { x: 360 }); // <-- automatically reverted
+//     },
+//     { scope: container }
+// );
 
   return (
     <nav className='nav'>
-      <div class="bg-gray-100 font-sans w-full m-0">
-        <div class="bg-white">
-          <div class="container mx-auto px-4">
-            <div class="flex  items-center justify-between py-4">
+      <div className="bg-gray-100 font-sans w-full m-0">
+        <div className="bg-white">
+          <div className="container mx-auto px-4">
+            <div className="flex items-center justify-between py-4">
               {/* logo & title */}
               <div>
-                <BsCupHot class="inline pr-2 pb-0.5" size={25} color="black" />
-                <h2 class="inline">Taiwanese in the UK</h2>
+                <BsCupHot className="inline pr-2 pb-0.5" size={25} color="black" />
+                <h2 className="inline">Taiwanese in the UK</h2>
+                
               </div>
 
               {/* choices */}
-              <div class="hidden sm:flex sm:items-center">
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">找房源</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">找二手物品</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">找旅伴</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500">聊聊天</a>
+              <div className="hidden sm:flex sm:items-center">
+                <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">找房源</a>
+                <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">找二手物品</a>
+                <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">找旅伴</a>
+                <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500">聊聊天</a>
               </div>
 
               {/* sign in */}
-              <div class="hidden sm:flex sm:items-center">
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">Sign in</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg text-teal-500 border-teal-500 hover:bg-teal-500 hover:text-white">Sign up</a>
+              <div className="hidden sm:flex sm:items-center">
+                <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">Sign in</a>
+                <a href="#" className="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg text-teal-500 border-teal-500 hover:bg-teal-500 hover:text-white">Sign up</a>
               </div>
 
               {/* hamburger icon */}
-              <div class="sm:hidden cursor-pointer" id="hamburger">
-                <a href=""><PiHamburgerThin class="inline pr-2 pb-0.5" size={25} color="black" /></a>
+              <div className="sm:hidden cursor-pointer" id="hamburger">
+                <button onClick={togglePhoneMenu}>
+                  <PiHamburgerThin className="inline pr-2 pb-0.5" size={25} color="black" />
+                </button>
               </div>
-
             </div>
-            
-            <div class="block sm:hidden bg-pink text-blue border-t-2 py-2" id="menuChoice">
-              <p>test</p>
-              <div class="flex flex-col">
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Products</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Marketplace</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Partners</a>
-                <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Pricing</a>
-                <div class="flex justify-between items-center border-t-2 pt-2">
-                  <a href="#" class="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">Sign in</a>
-                  <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-teal-600 hover:border-teal-600">Sign up</a>
+
+            {/* phone menu */}
+            {isPhoneMenuVisible && (
+              <div className="block sm:hidden bg-pink text-blue border-t-2 py-2" id="phoneMenuChoice">
+                <div className="flex flex-col">
+                  <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Products</a>
+                  <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Marketplace</a>
+                  <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Partners</a>
+                  <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mb-1">Pricing</a>
+                  <div className="flex justify-between items-center border-t-2 pt-2">
+                    <a href="#" className="text-gray-800 text-sm font-semibold hover:text-teal-500 mr-4">Sign in</a>
+                    <a href="#" className="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-teal-600 hover:border-teal-600">Sign up</a>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
